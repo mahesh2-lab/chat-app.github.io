ae10229bd8714bf6e85c49f9a12fe09147287c37
Mahesh Chopade <maheshchopade133@gmail.com>
2024-03-02 19:00:00 +0530

    FRONT END COMPLETED

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Message from "./Message";
+
+const Messages = () => {
+  return (
+    <div className="py-4 px-3 flex-1 overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+
+export default Messages;
