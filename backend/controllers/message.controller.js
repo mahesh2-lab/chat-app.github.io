f42f1ec62ec8476649d908a905f5df833ff97b90
Mahesh Chopade <maheshchopade133@gmail.com>
2024-03-02 10:36:59 +0530

    1st part is completed as baclend

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,58 @@
+import Conversation from "../models/coversation.model.js";
+import Message from "../models/massage.model.js"; // Import the Message model
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { id: receiverId } = req.params;
+    const { message } = req.body;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+
+    await Promise.all([newMessage.save(), conversation.save()]);
+
+    res.status(201).json({ message: "Message sent successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getMessages = async (req, res) => {
+try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+        participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    if (!conversation) {
+        return res.status(200).json([]);
+    }
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+
+} catch (error) {
+    console.log("error in getMessages ", error);
+    return res.status(500).json({ message: error.message });
+}
+};
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongodb from "./db/connectToMongo.js";
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  connectToMongodb();
+  console.log("server is running pn port", PORT);
+});
